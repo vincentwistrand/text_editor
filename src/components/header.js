9186f424c6f2e5c5ba25b87d2d6df357ec498eab
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-09-08 16:05:10 +0200

    Change header variable back to function

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,10 @@
-export default function Header() {
+import '../css/App.css';
+import React from 'react';
+
+function Header() {
     return  <div className='header'>
                 <h1>Foogle docs</h1>
             </div>
-}
+};
+
+export default Header;
