adf37312a23e292e66d8c0060bad4a21673838d2
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-09-16 19:45:57 +0200

    Add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './css/App.css';
 
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Docs from "./components/docs"
 import TextEditor from "./components/texteditor"
 
 function App() {
     return (
+      <Router>
         <Routes>
           <Route path="/" element={<Docs />} />
           <Route path="/texteditor" element={<TextEditor />} />
         </Routes>
+      </Router>
     );
 }
 
