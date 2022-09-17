a98b9b78cc81194bad0686ecdc4f4edeb82f0754
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-09-17 13:02:42 +0200

    Add new tests

--- src/test/texteditor.test.js
+++ src/test/texteditor.test.js
@@ -2,10 +2,36 @@ import { render, screen } from '@testing-library/react';
 import TextEditor from '../components/texteditor';
 import {HashRouter as Router} from 'react-router-dom';
 
-test('renders save button', () => {
+const document = 
+    {
+      _id: "6320a5a7129ba599acb3e8f2",
+      name: "Doc 1",
+      content: "<h1>Halloj</h1>"
+    };
+
+test('should contain document name', () => {
   render(<Router>
-            <TextEditor />
+            <TextEditor testDoc={document} />
           </Router>);
-  const createButton = screen.getByRole("button", { name: /Spara/i });
-  expect(createButton).toBeInTheDocument();
+  
+  const option1 = screen.getByText("Doc 1");
+  expect(option1).toBeInTheDocument();
 });
+
+test('should contain document content', () => {
+    render(<Router>
+              <TextEditor testDoc={document} />
+            </Router>);
+  
+    const option2 = screen.getByText("Halloj");
+    expect(option2).toBeInTheDocument();
+  });
+
+test('renders save button', () => {
+    render(<Router>
+              <TextEditor testDoc={document} />
+            </Router>);
+  
+    const createButton = screen.getByRole("button", { name: /Spara/i });
+    expect(createButton).toBeInTheDocument();
+  });
