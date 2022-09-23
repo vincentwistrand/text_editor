86d739feca7862b60c816efa34228fe5e827757a
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-09-23 11:35:38 +0200

    Change return values to result.data

--- src/models/docs.js
+++ src/models/docs.js
@@ -3,9 +3,10 @@
 const docsModel = {
     getAllDocs: async function getAllDocs() {
         const response = await fetch(`https://jsramverk-editor-viai20.azurewebsites.net/docs`);
+        //const response = await fetch(`http://localhost:1337/docs`);
         const result = await response.json();
 
-        return result;
+        return result.data;
     },
     saveDoc: async function saveDoc(doc) {
         const requestOptions = {
@@ -14,20 +15,22 @@ const docsModel = {
         const query = `id=${doc.id}&name=${doc.name}&content=${doc.content}`;
         
         const response = await fetch(`https://jsramverk-editor-viai20.azurewebsites.net/docs?${query}`, requestOptions);
+        //const response = await fetch(`http://localhost:1337/docs?${query}`, requestOptions);
         const result = await response.json();
 
-        return result;
+        return result.data;
     },
     createDoc: async function createDoc(doc) {
         const requestOptions = {
             method: 'POST'
         };
-        const query = `name=${doc.name}&content=${doc.content}`;
+        const query = `name=${doc.name}`;
         
         const response = await fetch(`https://jsramverk-editor-viai20.azurewebsites.net/docs?${query}`, requestOptions);
+        //const response = await fetch(`http://localhost:1337/docs?${query}`, requestOptions);
         const result = await response.json();
 
-        return result;
+        return result.data;
     },
     deleteDoc: async function deleteDoc(doc) {
         const requestOptions = {
@@ -36,6 +39,7 @@ const docsModel = {
         const query = `id=${doc.id}`;
         
         await fetch(`https://jsramverk-editor-viai20.azurewebsites.net/docs?${query}`, requestOptions);
+        //await fetch(`http://localhost:1337/docs?${query}`, requestOptions);
 
     }
 };
