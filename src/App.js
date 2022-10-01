8574e24a5564a0f2779e47f3ecba0403feb10d2c
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-10-02 00:37:28 +0200

    Add Start and Login routes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Docs from "./components/docs"
 import TextEditor from "./components/texteditor"
+import Login from "./components/login"
+import Start from "./components/start"
 
 function App() {
     return (
       <Router>
         <Routes>
-          <Route path="/" element={<Docs />} />
+          <Route path="/" element={<Start />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/documents" element={<Docs />} />
           <Route path="/texteditor" element={<TextEditor />} />
         </Routes>
       </Router>
